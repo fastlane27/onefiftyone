078a535c525a6630c2f6939f1e9471b8bea1c682
David Lane <fastlane27@gmail.com>
2020-04-20 07:46:52 -0500

    comment style tweaks

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -12,26 +12,28 @@ function Comment(props) {
   const dateCreated = new Date(props.comment.createdAt);
 
   return (
-    <div className={styles.comment}>
-      <div className={styles.name}>
-        <h3>{props.comment.createdBy.name}</h3>
-        {props.currentUser && props.currentUser._id === props.comment.createdBy._id &&
-          <button onClick={() => toggleEdit()}>Edit</button>
+    <div className={styles.container}>
+      <div className={styles.comment}>
+        <div className={styles.name}>
+          <h3>{props.comment.createdBy.name}</h3>
+          {props.currentUser && props.currentUser._id === props.comment.createdBy._id &&
+            <button onClick={() => toggleEdit()}>Edit</button>
+          }
+        </div>
+        {isVisible ?
+          <CommentEdit
+            comment={props.comment}
+            handleEditComment={props.handleEditComment}
+            handleDeleteComment={props.handleDeleteComment}
+            toggleEdit={toggleEdit}
+          />
+        :
+          <>
+            <p className={styles.content}>{props.comment.content}</p>
+            <p className={styles.date}>{dateCreated.toLocaleString()}</p>
+          </>
         }
       </div>
-      {isVisible ?
-        <CommentEdit
-          comment={props.comment}
-          handleEditComment={props.handleEditComment}
-          handleDeleteComment={props.handleDeleteComment}
-          toggleEdit={toggleEdit}
-        />
-      :
-        <>
-          <p className={styles.content}>{props.comment.content}</p>
-          <p className={styles.date}>{dateCreated.toLocaleString()}</p>
-        </>
-      }
     </div>
   );
 }
